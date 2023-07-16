ac06f46156febb530aae25af40e8066676f4fb5d
Herb <82109216+nightlyherb@users.noreply.github.com>
2023-07-16 09:50:19 +0900

    setup: first project (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,13 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Frontend Mentor Challenges</h1>
 
       <div>
-        <a href="./vite-mpa-test/">Link to vite mpa setup page</a>
+        <a href="./product-preview-card-component/">Product preview card component</a>
       </div>
-
-      <div>
-        <a href="./invalid-path/">Invalid Link</a>
-      </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
